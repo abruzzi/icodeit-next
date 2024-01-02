53f1372a388235a7f9dc7825263f6053cd5ff280
Juntao Qiu <juntao.qiu@gmail.com>
2024-01-02 19:26:09 +1100

    making sure we only show subscribe when we need to

--- components/subscribe.tsx
+++ components/subscribe.tsx
@@ -1,8 +1,24 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 
 import dynamic from "next/dynamic";
 import { Spinner } from "@nextui-org/react";
 
-export const Subscribe = dynamic(() => import("./async-sub-stack"), {
+import { InView } from "react-intersection-observer";
+
+export const Component = dynamic(() => import("./async-sub-stack"), {
   loading: () => <Spinner />,
 });
+
+export const Subscribe = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <InView onChange={(inView, entry) => setIsVisible(inView)}>
+      {({ inView, ref, entry }) => (
+        <div ref={ref}>{isVisible && <Component />}</div>
+      )}
+    </InView>
+  );
+};
